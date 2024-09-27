4fecf1b240d8c86f4574029741f8cc2c24e5b4de
datngth03 <datngth03@gmail.com>
2024-09-27 19:18:33 +0700

    #fix file vite.config

--- app.client/vite.config.ts
+++ app.client/vite.config.ts
@@ -1,65 +1,61 @@
 ﻿import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-export default defineConfig(({ mode }) => {
-    // Chỉ dùng HTTPS trong môi trường phát triển
-    const isDevelopment = mode === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
 
-    // Cấu hình chung cho server
-    const serverConfig: any = {
-        port: 5173,
+const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
+
+const certificateName = "app.client";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+// Chỉ thực hiện logic tạo chứng chỉ nếu không phải môi trường production
+if (!isProduction) {
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+        if (0 !== child_process.spawnSync('dotnet', [
+            'dev-certs',
+            'https',
+            '--export-path',
+            certFilePath,
+            '--format',
+            'Pem',
+            '--no-password',
+        ], { stdio: 'inherit', }).status) {
+            throw new Error("Could not create certificate.");
+        }
+    }
+}
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7215';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
         proxy: {
             '^/weatherforecast': {
-                target: isDevelopment ? (env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7215') : '',
-                secure: false,
+                target,
+                secure: false
             }
         },
-    };
-
-    // Nếu là môi trường phát triển, thêm cấu hình HTTPS
-    if (isDevelopment) {
-        const baseFolder =
-            env.APPDATA !== undefined && env.APPDATA !== ''
-                ? `${env.APPDATA}/ASP.NET/https`
-                : `${env.HOME}/.aspnet/https`;
-
-        const certificateName = "app.client";
-        const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-        const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-        if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-            if (0 !== child_process.spawnSync('dotnet', [
-                'dev-certs',
-                'https',
-                '--export-path',
-                certFilePath,
-                '--format',
-                'Pem',
-                '--no-password',
-            ], { stdio: 'inherit', }).status) {
-                throw new Error("Could not create certificate.");
-            }
-        }
-
-        // Thêm cấu hình HTTPS vào serverConfig
-        serverConfig.https = {
+        port: 5173,
+        https: !isProduction ? {
             key: fs.readFileSync(keyFilePath),
             cert: fs.readFileSync(certFilePath),
-        };
+        } : undefined // Không sử dụng HTTPS trong môi trường production
     }
-
-    return {
-        plugins: [plugin()],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            }
-        },
-        server: serverConfig,
-    };
 });
--- app.server/Program.cs
+++ app.server/Program.cs
@@ -108,7 +108,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
     {
-        builder.WithOrigins("https://localhost:5173", "http://localhost:5173")
+        builder.WithOrigins("https://localhost:5173", "http://localhost:5173", "https://fin-app-eight.vercel.app")
                .AllowAnyHeader()
                .AllowAnyMethod()
                .AllowCredentials();
